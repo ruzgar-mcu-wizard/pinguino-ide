940eba18952dd40d618a9feb0dacf0177408f4c9
Rüzgar The MCU Wizard <ruzgarefecelik67@gmail.com>
2026-05-10 13:39:05 +0300

    Delete pinguino.sh

--- pinguino.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Launch Pinguino's IDE
-echo "Launch Pinguino's IDE ..."
-cd ~/Pinguino/v13/pinguino-ide/
-python3 -m pip install pipenv
-python3 -m pipenv install
-python3 -m pipenv run python pinguino-ide.py
